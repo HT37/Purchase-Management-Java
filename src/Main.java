468d9afd901da0f8d45c513a00a5252db59450c2
HT37 <harshfootball10@gmail.com>
2021-04-15 21:18:25 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Jai Git Hub");
-System.out.println("KAAM 25 HAI UPENDRA KA");
 	}
 
 }
